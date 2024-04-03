f1f9d743c85445ad6c0ac7afc4e85f15d3a71aac
Alexander Fedyushin <fedyushinap@gmail.com>
2024-04-03 18:16:10 +0300

    add apps

---
+++ Desktop/Prog.Academy/django_april_2024/app_1/views.py
@@ -0,0 +1,9 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def app_1(request):
+    return HttpResponse('<h1>App 1<h2/>')
+
+def index(request):
+    return HttpResponse('<h1>Index<h2/>')
